a6c83670966d3768a3f3258af9d7d998264398dd
takaxis <axis1130@gmail.com>
2021-07-26 00:52:41 +0900

    router setup'

---
+++ src/components/Router.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+
+const AppRouter = () => {
+  const [isLoggedIn, setLoggedIn] = useState(true);
+  return (
+    <Router>
+      <Switch>
+        {isLoggedIn ? (
+          <>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </>
+        ) : (
+          <Route exact path="/">
+            <Auth />
+          </Route>
+        )}
+      </Switch>
+    </Router>
+  );
+};
+
+export default AppRouter;
+
+///<>는 많은 요소들을 render하고 싶고 부모요소가 없을때 사용,
+/// div나 span 없이도 사용 가능
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import firebase from "./firebase";
+import App from "./components/App";
 
 //import reportWebVitals from "./reportWebVitals";
 
@@ -11,5 +10,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-//reportWebVitals();
