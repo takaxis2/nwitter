5b2c591b908f8dbffb8d413322efbe0baa954b48
takaxis <axis1130@gmail.com>
2021-07-27 10:50:58 +0900

    Log In

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AppRouter from "./Router";
 import { authService } from "../fbase";
 
 function App() {
-  const [isLoggedIn, setLoggedIn] = useState(authService.currentUser);
+  const [init, setinit] = useState(false);
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setLoggedIn(true);
+      } else {
+        setLoggedIn(false);
+      }
+      setinit(true);
+    });
+  }, []);
+
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing"}
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
